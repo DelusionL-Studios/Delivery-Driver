353a93f375baeea8efae946f41f82bde9edf61ff
Tristan Hedges <tristan.hedges96@yahoo.com>
2024-10-30 22:47:51 -0400

    ID1 : Movement
    - Added Sandbox scene for testing things
    - Added primative sprite to Level_1
    - Added player control to primative sprite

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float turnSpeed = 0f;
+    [SerializeField] float movementSpeed = 0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Move();
+        Turn();
+    }
+
+    void Move()
+    {
+        var movementInput = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        transform.Translate(0, movementInput, 0);
+    }
+
+    void Turn()
+    {
+        var turnInput = Input.GetAxis("Horizontal") * -turnSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, turnInput);
+    }
+    //update
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Oh no this isn't working...");
+        Debug.Log("Well this still isn't working...");
+        Debug.Log("Maybe now?");
+        Debug.Log("Definitely working now! :D");
+        Debug.Log("My name is " + this.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
